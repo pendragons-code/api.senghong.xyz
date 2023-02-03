1c5725f5aeb2c9d66a20eb45701ef98ecad6ee98
pendragons-code <pendragonscode@gmail.com>
2023-02-04 03:13:07 +0800

    figuring this out

--- src/loaders/apiRoutes.js
+++ src/loaders/apiRoutes.js
@@ -11,8 +11,11 @@ readdirSync(`./src/api/`).forEach(async (dirs) => {
 	for await (const file of loadApiFile) {
 		const { execute, name } = await require(`../api/${dirs}/${file}`)
 		let endpointsInArray = await db.get("endpointsInArray")
-		if(endpointsInArray === null || !endpointsInArray.includes(name)) await db.push("endpointsInArray", name)
-
+		if(endpointsInArray === null || !endpointsInArray.includes(name)){
+			await db.push("endpointsInArray", name)
+			console.log(name)
+			console.log(endpointsInArray)
+		}
 		routerAPI.post(`/${name}`, async (req, res) => {
 			if(!req.body) return res.json({ error: "You need to provide a json request!" })
 			execute(req, res)
