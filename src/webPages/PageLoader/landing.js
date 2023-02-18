b9ab95ad11139855edb8a3327dc21199b0d0d38f
pendragons-code <pendragonscode@gmail.com>
2023-02-19 00:42:24 +0800

    ok fine

--- src/webPages/PageLoader/landing.js
+++ src/webPages/PageLoader/landing.js
@@ -1,8 +1,11 @@
 const { db } = require("../../loaders/dataBase.js")
+const { domain } = require("../../../configuration/hosting.json")
+const env = require("dotenv").config()
+const port = process.env.port
 module.exports = {
 	name: "/",
 	async execute(req, res) {
 		await db.add("SuccessfulRequestCounter", 1)
-		res.render("landingPage.ejs")
+		res.render("landingPage.ejs", { domain: `${domain}${port}` })
 	}
 }
