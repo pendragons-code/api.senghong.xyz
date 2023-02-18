979bbd700108fbea58d03db002b38f34d23b7f0f
pendragons-code <pendragonscode@gmail.com>
2023-02-18 19:21:33 +0800

    Yeah....................................

--- src/webPages/PageLoader/landing.js
+++ src/webPages/PageLoader/landing.js
@@ -3,7 +3,6 @@ module.exports = {
 	name: "/",
 	async execute(req, res) {
 		await db.add("SuccessfulRequestCounter", 1)
-		console.log(await db.get("endpointsInCategory"))
 		res.render("landingPage.ejs")
 	}
 }
