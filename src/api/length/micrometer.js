a57035e4891d30e6c5caa5e0a0749699abe0114b
pendragons-code <pendragonscode@gmail.com>
2023-02-22 21:51:00 +0800

    I lost my calculator :(

--- src/api/length/micrometer.js
+++ src/api/length/micrometer.js
@@ -1,7 +1,7 @@
 const { noNumberProvided, noJsonProvided } = require("../../../assets/errorMessages.json")
 const { db } = require("../../loaders/dataBase.js")
 module.exports = {
-	name: "",
+	name: "micrometer",
 	category: "length",
 	utilisation: `
 	{
@@ -13,5 +13,28 @@ module.exports = {
 		if(isNaN(req.body.requestedLength)) return res.json({ error: noNumberProvided })
 		await db.add(`SuccessfulRequestCounter`, 1)
 		let numberInMicrometer = parseFloat(req.body.requestedLength)
+		let numberInCentimeter = numberInMicrometer / 10000
+		let numberInMeter = numberInMicrometer / 1000000
+		let numberInKilometer = numberInMicrometer / 1000000000
+		let numberInMillimeter = numberInMicrometer / 1000
+		let numberInNanometer = numberInMicrometer * 1000
+		let numberInMile = numberInMicrometer * 6.2137e-10
+		let numberInYard = numberInMicrometer / 914400
+		let numberInFoot = numberInMicrometer / 304800
+		let numberInInch = numberInMicrometer / 25400
+		let numberInNauticalMile = numberInMicrometer * 5.3996e-10
+		return res.json({
+			requested: numberInMicrometer,
+			Centimeter: numberInCentimeter,
+			Meter: numberInMeter,
+			Kilometer: numberInKilometer,
+			Millimeter: numberInMillimeter,
+			Nanometer: numberInNanometer,
+			Mile: numberInMile,
+			Yard: numberInYard,
+			Foot: numberInFoot,
+			Inch: numberInInch,
+			NauticalMile: numberInNauticalMile
+		})
 	}
 }
