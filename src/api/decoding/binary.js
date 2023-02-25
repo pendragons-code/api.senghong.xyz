df7cff3edd5fae4f243da2e43db4a35762338953
pendragons-code <pendragonscode@gmail.com>
2023-02-26 06:51:06 +0800

    Decided to add this thing because I aw it. I might change the limit tho.

--- src/api/decoding/binary.js
+++ src/api/decoding/binary.js
@@ -1,8 +1,8 @@
 const { invalidRequest, maximumLength, noStringProvided } = require("../../../assets/errorMessages.json")
 const { db } = require("../../loaders/dataBase.js")
 module.exports = {
-	name: "encodeBase64",
-	categoty: "encoding",
+	name: "decodeBinary",
+	category: "encoding",
 	utilisation: `
 	{
 		requestedString: "ILikeApples"
@@ -10,15 +10,11 @@ module.exports = {
 	`,
 	async execute(req, res) {
 		if(!req.body.requestedString) return res.json({ error: invalidRequest })
-		if(req.body.requestedString.length > 100000) return res.json({ error: maximumLength, maximumAllowedLength: 100000 })
+		if(req.body.requestedString > 100000) return res.json({ error: maximumLength, maximumAllowedLength: 100000 })
 		if(typeof req.body.requestedString !== "string") return res.json({ error: noStringProvided })
 		await db.add(`SuccessfulRequestCounter`, 1)
-		let bufferObject = new Buffer.from(req.body.requestedString)
-		let dataInBase64 = bufferObject.toString("base64")
-		res.json({
-			requested: req.body.requestedString,
-			base64Output: dataInBase64
-		})
-		return bufferObject.fill(0)
+		let seperateByWhitespace = req.body.requestedString.split(" ")
+		let notBinaryResult = seperateByWhitespace.map(binary => String.fromCharCode(parseInt(binary, 2))).join("")
+		return res.json({ requested: req.body.requestedString, textResult: notBinaryResult })
 	}
 }
---
+++ src/api/encoding/binary.js
@@ -0,0 +1,24 @@
+const { invalidRequest, maximumLength, noStringProvided } = require("../../../assets/errorMessages.json")
+const { db } = require("../../loaders/dataBase.js")
+module.exports = {
+	name: "encodeBinary",
+	category: "encoding",
+	utilisation: `
+	{
+		requestedString: "ILikeApples"
+	}
+	`,
+	async execute(req, res) {
+		if(!req.body.requestedString) return res.json({ error: invalidRequest })
+		if(req.body.requestedString > 100000) return res.json({ error: maximumLength, maximumAllowedLength: 100000 })
+		if(typeof req.body.requestedString !== "string") return res.json({ error: noStringProvided })
+		await db.add(`SuccessfulRequestCounter`, 1)
+		let resultInBinary = ""
+		for(let i = 0; i < req.body.requestedString.length; ++i) {
+			let bin = req.body.requestedString[i].charCodeAt().toString(2)
+			resultInBinary += Array(8 - bin.length + 1).join("0") + bin
+			if(i < (req.body.requestedString.length - 1)) resultInBinary += " "
+		}
+		return res.json({ requested: req.body.requestedString, binaryOutput: resultInBinary })
+	}
+}
