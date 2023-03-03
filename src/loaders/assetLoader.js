1344b902ecfaa7228bc5bcc4711e5a19c6b3abc0
pendragons-code <pendragonscode@gmail.com>
2023-03-03 22:03:58 +0800

    notifs better

--- src/loaders/assetLoader.js
+++ src/loaders/assetLoader.js
@@ -1,7 +1,7 @@
 const { readFile } = require("fs")
 async function checkAssets() {
 	readFile("./assets/rockyou.txt", "utf8", async function(err, data){
-		if(!data || err) return console.log("\n[ALERT: MISSING ASSETS!] ROCKYOU DOES NOT EXIST IN THE EXPECTED LOCATION!!!!\n")
+		if(!data || err) return console.log("\n[ALERT: MISSING ASSETS!] 'rockyou.txt' DOES NOT EXIST IN THE EXPECTED LOCATION!!!!\n")
 	})
 }
 checkAssets()
