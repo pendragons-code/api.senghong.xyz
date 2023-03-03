897d6f950db859d81f87bdbaa818935679b81104
pendragons-code <pendragonscode@gmail.com>
2023-03-03 21:59:16 +0800

    Just some basic url manipulation for documentation.

--- src/webPages/PageLoader/EndPointLister.js
+++ src/webPages/PageLoader/EndPointLister.js
@@ -1,10 +1,19 @@
 const { db } = require("../../loaders/dataBase.js")
+const { domain } = require("../../../configuration/hosting.json")
 const { codeRequestSampleOne, codeRequestSampleTwo } = require("../../../assets/codeRequestSample.js")
+const env = require("dotenv").config()
 module.exports = {
 	name: "EndPoints",
 	async execute(req, res) {
 		await db.add("SuccessfulRequestCounter", 1)
 		let endpointsInArray = await db.get("endpointsInArray")
-		res.render("EndPoints.ejs", { endpoints: `/api/${endpointsInArray.join("<br>/api/")}`, codeRequestSample: `${codeRequestSampleOne}<br>   requestedTemperature: 15<br>${codeRequestSampleTwo}` })
+		let listOfEndpoints = ""
+		let port = process.env.port
+		for (let i = 0; i < endpointsInArray.length; ++i){
+			let endpointsInArrayWithoutCategoryByPosition = endpointsInArray[i].search(" -")
+			let endpointsInArrayWithoutCategory = endpointsInArray[i].slice(0, endpointsInArrayWithoutCategoryByPosition)
+			listOfEndpoints += `<a href="${domain}${port}/${endpointsInArrayWithoutCategory}">${endpointsInArray[i]}</a><br>`
+		}
+		return res.render("EndPoints.ejs", { endpoints: listOfEndpoints, codeRequestSample: `${codeRequestSampleOne}<br>   requestedTemperature: 15<br>${codeRequestSampleTwo}` })
 	}
 }
