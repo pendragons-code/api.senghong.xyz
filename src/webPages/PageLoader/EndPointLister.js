886150bf7b88931d9fe14772a39df0d65d6ebf45
pendragons-code <pendragonscode@gmail.com>
2023-03-03 22:06:06 +0800

    /api missing

--- src/webPages/PageLoader/EndPointLister.js
+++ src/webPages/PageLoader/EndPointLister.js
@@ -12,7 +12,7 @@ module.exports = {
 		for (let i = 0; i < endpointsInArray.length; ++i){
 			let endpointsInArrayWithoutCategoryByPosition = endpointsInArray[i].search(" -")
 			let endpointsInArrayWithoutCategory = endpointsInArray[i].slice(0, endpointsInArrayWithoutCategoryByPosition)
-			listOfEndpoints += `<a href="${domain}/${endpointsInArrayWithoutCategory}">${endpointsInArray[i]}</a><br>`
+			listOfEndpoints += `<a href="${domain}/api/${endpointsInArrayWithoutCategory}">${endpointsInArray[i]}</a><br>`
 		}
 		return res.render("EndPoints.ejs", { endpoints: listOfEndpoints, codeRequestSample: `${codeRequestSampleOne}<br>   requestedTemperature: 15<br>${codeRequestSampleTwo}` })
 	}
